9a87155e4d4923ba0e957a7e2e9e1c9cdd91cd73
ftnext <takuyafjp+develop@gmail.com>
2025-09-25 19:34:39 +0900

    [feat] Resemble stdlib logging format

--- samplecode/python-logging/library_logger/structlog_example.py
+++ samplecode/python-logging/library_logger/structlog_example.py
@@ -6,15 +6,35 @@
 #     "structlog",
 # ]
 # ///
+import datetime
 import logging
 
 import httpx
 import structlog
 from rich.pretty import pprint
 
+
+def add_millisecond_timestamp(logger, name, event_dict):
+    dt = datetime.datetime.now()
+    event_dict["timestamp"] = f"{dt:%Y-%m-%d %H:%M:%S},{dt.microsecond // 1000:03d}"
+    return event_dict
+
+
 shared_processors = [
+    structlog.stdlib.add_logger_name,
     structlog.stdlib.add_log_level,
-    structlog.processors.TimeStamper(fmt="iso"),
+    structlog.stdlib.PositionalArgumentsFormatter(),
+    add_millisecond_timestamp,  # TimeStamper(fmt="iso")よりもstdlib loggingに寄せる
+    structlog.processors.StackInfoRenderer(),
+    structlog.processors.format_exc_info,
+    structlog.processors.UnicodeDecoder(),
+    structlog.processors.CallsiteParameterAdder(
+        {
+            structlog.processors.CallsiteParameter.FILENAME,
+            structlog.processors.CallsiteParameter.FUNC_NAME,
+            structlog.processors.CallsiteParameter.LINENO,
+        }
+    ),
 ]
 
 structlog.configure(
