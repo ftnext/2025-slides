d7100eb046a67f6b5a0ddddf274d91b05b67c5aa
ftnext <takuyafjp+develop@gmail.com>
2025-09-25 13:00:47 +0900

    [feat] loguru intercept example

---
+++ samplecode/python-logging/library_logger/loguru_example.py
@@ -0,0 +1,49 @@
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#     "httpx",
+#     "loguru",
+#     "rich",
+# ]
+# ///
+import inspect
+import logging
+
+import httpx
+from loguru import logger
+from rich.pretty import pprint
+
+
+# https://github.com/Delgan/loguru#entirely-compatible-with-standard-logging
+class InterceptHandler(logging.Handler):
+    def emit(self, record: logging.LogRecord) -> None:
+        try:
+            level: str | int = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        frame, depth = inspect.currentframe(), 0
+        while frame:
+            filename = frame.f_code.co_filename
+            is_logging = filename == logging.__file__
+            is_frozen = "importlib" in filename and "_bootstrap" in filename
+            if depth > 0 and not (is_logging or is_frozen):
+                break
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(
+            level, record.getMessage()
+        )
+
+
+logging.basicConfig(
+    handlers=[InterceptHandler()],
+    format="%(asctime)s | %(levelname)s (%(name)s) | %(filename)s:%(funcName)s:%(lineno)d - %(message)s",
+    level=logging.DEBUG,
+)
+logging.getLogger().handlers[0].addFilter(logging.Filter("httpx"))
+
+resp = httpx.get("https://peps.python.org/api/peps.json")
+data = resp.json()
+pprint([(k, v["title"]) for k, v in data.items()][:10])
